9cf56fbf12433aea8b8f781282a9375157e614c8
ZachRiddle <zriddle2026@kehillahstudent.org>
2025-10-05 17:34:31 -0700

    implemented last parens method

--- TruthAssignment.java
+++ TruthAssignment.java
@@ -9,6 +9,7 @@ public class TruthAssignment {
 
     public static String instantiate(String Prop, TruthAssignment Val) {
         String retval = "";
+        Prop = lastparen(Prop);
 
         for(int i = 0; i < Prop.length(); i++) {
             char eVal = Prop.charAt(i);
@@ -22,6 +23,17 @@ public class TruthAssignment {
         }
         return retval;
     }
+    public static String lastparen (String sentence) {
+        int i = -1;
+        int Hi = sentence.length();
+        if (sentence.contains("(")) {
+            i = sentence.lastIndexOf('(');
+            String firsthalf = sentence.substring(0, i+1);
+            String amend = sentence.substring(i+1, sentence.length());
+            if (sentence.contains(")")) Hi = firsthalf.length() + amend.indexOf(')');
+        }
+        return sentence = sentence.substring(i+1, Hi);
+    }
 public static void main (String[] args) {
     String Phi = "p&q";
     TruthAssignment Ta = new TruthAssignment("pq", "10");
@@ -29,6 +41,7 @@ public static void main (String[] args) {
     String actualTest2 = instantiate("qp pq", Ta);
     String actualTest3 = instantiate("pq \"none of these letters have a truth assignment\"", Ta);
     String actualTest4 = instantiate("pqpqpq", new TruthAssignment("pq", "10"));
-    System.out.println("(" + actual + ") (" + actualTest2 + ") (" + actualTest3 + ") (" + actualTest4 + ")");
+    String actualTest5 = instantiate("(pq)((pqpq)))", Ta);
+    System.out.println(" " + actual + " " + actualTest2 + " " + actualTest3 + " " + actualTest4 + " " + actualTest5);
     }
 }
